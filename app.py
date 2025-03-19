11c0027d9533e76ac1ef1b07c69b0fa752d49d8f
Allan PRET <allan.gonin-saget@etudiant.univ-rennes1.fr>
2025-03-19 16:03:25 +0100

    verif de l'id de session

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
 from flask import Flask
 
+import requests
+import datetime
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+PHP_SESSION_ID = os.getenv("PHPSESSID")
+
 app = Flask(__name__)
+
+
+LOG_FILE = "logs.txt"
+
+
+def log_message(message):
+    with open(LOG_FILE, "a") as f:
+        f.write(f"[{datetime.datetime.now()}] {message}\n")
 
 @app.route('/')
 def home():
-    return "<h1>Projet d'automatisation de la roue de la fortune !</h1>"
+    return f"<h1>Projet d'automatisation de la roue de la fortune !</h1>"
 
 @app.route('/check')
-def about():
-    return "<h1>Verifier les logs du bots (WIP)</h1>"
+def check_logs():
+    try:
+        with open(LOG_FILE, "r") as f:
+            logs = f.read().replace("\n", "<br>")
+        return f"<h1>Logs du bot :</h1><p>{logs}</p>"
+    except FileNotFoundError:
+        return "<h1>Aucun log pour le moment.</h1>"
 
 @app.route('/aucasou')
 def contact():
-    return "<h1>Page de au cas ou</h1>"
+    return f"<h1>Page de au cas ou => {PHP_SESSION_ID}</h1>"
 
 
 if __name__ == "__main__":
